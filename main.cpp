fffa567ab066c79fcaa50ac00470cce1abdbbbf6
JoaBerra <31992783+JoaBerra@users.noreply.github.com>
2018-11-30 17:55:36 +0100

    First test

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(){
    print_hello();
    //cout << "Hello World!";
-   cout << endl;
+   cout << endl; 
    cout << "The factorial of 5 is " << factorial(5) << endl;
    return 0;
 }
